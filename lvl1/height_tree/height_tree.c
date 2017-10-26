a613d4cf1d2cd47c45d8bc78614d1101eb0257f1
Indy <kdevlinmurray@gmail.com>
2017-10-25 20:35:58 -0700

    adding height tree, need to add main with tests

--- lvl1/height_tree/height_tree.c
+++ lvl1/height_tree/height_tree.c
@@ -1,4 +1,25 @@
+int	ft_max(int a, int b)
+{
+	return (a >= b ? a : b);
+}
+
+int	go_deeper(struct s_node *node, int height)
+{
+	int	max;
+	int i;
+	struct s_node **arr;
+
+	max = height;
+	arr = node->nodes;
+	i = -1;
+	while (arr[++i])
+		max = ft_max(max, go_deeper(arr[i], height + 1));
+	return (max);
+}
+
 int height_tree(struct s_node *root)
 {
-
+	if (!root)
+		return (-1);
+	return (go_deeper(root, 0));
 }
